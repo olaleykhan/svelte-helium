14c5a6b42a9392464ab8910855fc753b0c096733
olaleykhan <realjoy9@gmail.com>
2023-08-21 10:20:32 +0100

    init svelte, add tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<section>
+
+    <h1 class="text-4xl font-bold underline text-red-900 ">
+        Hello world! Begiining of svelte portfolio website
+      </h1>
+
+      <p class="text-2xl font-light text-orange-600"> 
+        where is the world when they need you ?
+
+      </p>
+</section>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.100);
+    }
+  </style>
